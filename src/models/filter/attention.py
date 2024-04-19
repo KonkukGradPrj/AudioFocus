29044d4f2688a68c4665ae21f55b6a5dbbc42fb3
hyeonsio <chohs9803@gmail.com>
2024-04-19 15:11:13 +0900

    04.19 hyeonseo add positional embedding in feature vector

--- src/models/filter/attention.py
+++ src/models/filter/attention.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import ipdb
 
 from einops import rearrange
 from einops.layers.torch import Rearrange
@@ -9,44 +10,60 @@ from src.models.filter.base import BaseFilter
 
 
 class CrossAttention(nn.Module):
-    def __init__(self, feat_dim=192, emb_dim=384, heads=6, dim_head=96):
+    def __init__(self, feat_dim=192, emb_dim=384, seq_len=1500, heads=6, alpha=4, dim_head=96):
         super().__init__()
         self.heads = heads
         self.scale = dim_head ** -0.5
 
-        self.to_q = nn.Linear(feat_dim, heads * dim_head, bias=False)
-        self.to_k = nn.Linear(emb_dim, heads * dim_head, bias=False)
-        self.to_v = nn.Linear(emb_dim, heads * dim_head, bias=False)
-        self.to_out = nn.Linear(heads * dim_head, emb_dim)
+        self.to_q = nn.Linear(feat_dim, heads * dim_head  // alpha, bias=False)
+        self.to_k = nn.Linear(emb_dim, heads * dim_head  // alpha, bias=False)
+        self.to_v = nn.Linear(emb_dim, heads * dim_head  // alpha, bias=False)
+        self.to_out = nn.Linear(heads * dim_head  // alpha, emb_dim // alpha)
+        self.apply(self._init_weights_norm)
 
+        self.register_buffer("positional_embedding", sinusoids(seq_len, feat_dim))
+
+        
+    def _init_weights_norm(self, m):
+        """
+        Initialize weights and biases normal distribution in the range with 1e-4 std.
+        """
+        if isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, mean=0.0, std=0.01)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+                
     def forward(self, emb, feat):
+        # Expand feat to match emb dimensions
         if feat.dim() == 2:
-            feat = feat.unsqueeze(1).expand(-1, emb.size(1), -1)  # Expand feat to match emb dimensions
+            feat = feat.unsqueeze(1).expand(-1, emb.size(1), -1) 
+            feat =(feat + self.positional_embedding).to(feat.dtype)        
         
         q = rearrange(self.to_q(feat), 'b n (h d) -> b h n d', h=self.heads)
         k = rearrange(self.to_k(emb), 'b n (h d) -> b h n d', h=self.heads)
         v = rearrange(self.to_v(emb), 'b n (h d) -> b h n d', h=self.heads)
-        # Compute attention scores and apply softmax
-        dots = torch.matmul(q, k.transpose(-2, -1)) * self.scale
+
+        dots = torch.matmul(q, k.transpose(-2, -1)) * self.scale # b, h, n, d
         attn = F.softmax(dots, dim=-1)
 
         # Compute the output from the attention and values
         out = torch.matmul(attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
+
         return self.to_out(out)
 
 
 class CrossAttentionBlock(nn.Module):
-    def __init__(self, feat_dim=192, emb_dim=384, n_head=4, drop_prob=0.1):
+    def __init__(self, feat_dim=192, emb_dim=384, seq_len=1500, n_head=3, alpha=4, drop_prob=0.1):
         super().__init__()
         self.dropout1 = nn.Dropout(p=drop_prob)
-        self.norm1 = nn.LayerNorm(emb_dim)
+        self.norm1 = nn.LayerNorm(emb_dim // alpha)
         self.dropout2 = nn.Dropout(p=drop_prob)
         self.norm2 = nn.LayerNorm(emb_dim)
         
-        self.cross_attention = CrossAttention(feat_dim, emb_dim, heads=n_head, dim_head=emb_dim // n_head)
-        self.ffn = nn.Sequential(nn.Linear(emb_dim, emb_dim), nn.ReLU(),
-                                 nn.Linear(emb_dim, emb_dim), nn.ReLU(), nn.Dropout(p=drop_prob))
+        self.cross_attention = CrossAttention(feat_dim, emb_dim, seq_len=seq_len, heads=n_head, alpha=alpha, dim_head=emb_dim // n_head)
+        self.ffn = nn.Sequential(nn.Linear(emb_dim // alpha, emb_dim // alpha), nn.ReLU(),
+                                 nn.Linear(emb_dim // alpha, emb_dim), nn.ReLU(), nn.Dropout(p=drop_prob))
         
     def forward(self, emb, feat):
         emb = self.cross_attention(emb, feat)
@@ -63,30 +80,20 @@ class CrossAttentionBlock(nn.Module):
 
 class AttentionFilter(BaseFilter):
     name = 'attention'
-    def __init__(self, feat_dim=192, emb_dim=384, seq_len=1500, n_head=6, drop_prob=0.1):
+    def __init__(self, feat_dim=192, emb_dim=384, seq_len=1500, n_head=3, alpha=4, drop_prob=0.1):
         super().__init__()
 
         self.dropout0 = nn.Dropout(p=drop_prob)
         self.ln0 = nn.LayerNorm(emb_dim)
         
         self.blocks = nn.ModuleList([
-            CrossAttentionBlock(feat_dim, emb_dim, n_head, drop_prob),
-            CrossAttentionBlock(feat_dim, emb_dim, n_head, drop_prob),
-            CrossAttentionBlock(feat_dim, emb_dim, n_head, drop_prob),
-            CrossAttentionBlock(feat_dim, emb_dim, n_head, drop_prob)
+            CrossAttentionBlock(feat_dim, emb_dim, seq_len, n_head, alpha, drop_prob),
+            CrossAttentionBlock(feat_dim, emb_dim, seq_len, n_head, alpha, drop_prob),
+            CrossAttentionBlock(feat_dim, emb_dim, seq_len, n_head, alpha, drop_prob),
+            CrossAttentionBlock(feat_dim, emb_dim, seq_len, n_head, alpha, drop_prob),
         ])
 
-        self.register_buffer("positional_embedding", sinusoids(seq_len, emb_dim))
 
-        
-    def _init_weights_gaus(self, m):
-        """
-        Initialize weights and biases uniformly in the range [-1e-4, 1e-4].
-        """
-        if isinstance(m, nn.Linear):
-            nn.init.uniform_(m.weight, mean=0.0, std=0.03)
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
 
 
     def forward(self, emb, feat, idx=-1):
